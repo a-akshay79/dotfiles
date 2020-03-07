c0ab869af5d33b975dd875bc8516622438fbf727
a-akshay79 <56759311+a-akshay79@users.noreply.github.com>
2020-03-07 22:08:50 +0530

    Update ricesetup.sh

--- ricesetup.sh
+++ ricesetup.sh
@@ -2,7 +2,7 @@
 
 sudo apt install i3-gaps i3status i3bar i3 lock dunst rofi dmenu scrot feh rxvt-unicode xsettingsd xorg-xbacklight xclip lxappearance xsel
 
-git clone https://github.com/a-akshay79/dotfiles.githttps://github.com/a-akshay79/dotfiles.git
+git clone https://github.com/a-akshay79/dotfiles.git
 
 cd dotfiles
 
